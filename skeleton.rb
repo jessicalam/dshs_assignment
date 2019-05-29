8e7b136fbf05d9decf07cca8f502f8b44d527d10
jessicalam <j.lam@mail.com>
2019-05-29 16:31:58 -0400

    basic skeleton

---
+++ skeleton.rb
@@ -0,0 +1,26 @@
+require 'tty-prompt'
+
+def add_service
+
+end
+
+def remove_service
+
+end
+
+def add_service_provider
+
+end
+
+def remove_service_provider
+
+end
+
+def schedule_appointment
+
+end
+
+def schedule_availability
+
+end
+
