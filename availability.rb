4d3be89031ffae4062f4327a144e57c65667bdbf
jessicalam <j.lam@mail.com>
2019-05-31 15:03:58 -0400

    Update availability.rb

--- availability.rb
+++ availability.rb
@@ -16,7 +16,7 @@ def prompt_add_av
     if add_reoccur == 'yes' or add_reoccur ==  'y'
         DshsData.instance.add_service_provider(add_service_provider, add_service_time, add_reoccur)
         #if one-off, add to availability_blocks array
-    elsif add_reoccur == 'no' or add_reoccur ==  'n'
+    else
         DshsData.instance.add_availability(add_service_provider, add_service_time, start_time, end_time, add_availability)
 
     end
@@ -37,8 +37,9 @@ def prompt_remove_av
     end_time = 60
     if requested_reoccur == 'yes' or requested_reoccur == 'y' #if reoccurring, edit the specified service provider's availability dictionary within the service_providers dictionary
         DshsData.instance.remove_service_provider(requested_service_provider, requested_service_time, requested_reoccur)
-    elsif requested_reoccur == 'no' or requested_reoccur == 'n'  #if one-off, prompt for which date? and add to availability_blocks array
+    else  #if one-off, prompt for which date? and add to availability_blocks array
         DshsData.instance.remove_availability(requested_service_provider, requested_service_time, start_time, end_time)
+
     end
 
 
